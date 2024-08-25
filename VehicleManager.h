c1c20fc51d62bd6d2a661c4b1b852d33894bdd5c
Jonathan Steele <iNoles@users.noreply.github.com>
2024-08-24 22:53:17 -0400

    Update VehicleManager.h

--- VehicleManager.h
+++ VehicleManager.h
@@ -3,19 +3,44 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <memory>
 
+// Vehicle class to represent each vehicle
+class Vehicle {
+private:
+    std::string name;
+    std::string type;
+
+public:
+    Vehicle(const std::string& name, const std::string& type) : name(name), type(type) {}
+
+    std::string getName() const {
+        return name;
+    }
+
+    std::string getType() const {
+        return type;
+    }
+
+    void display() const {
+        std::cout << "Vehicle: " << name << " | Type: " << type << std::endl;
+    }
+};
+
+// Singleton VehicleManager class
 class VehicleManager {
 private:
     VehicleManager() {} // Private constructor
-
-    static VehicleManager* instance; // Private instance of the class
+    static VehicleManager* instance;
+    std::vector<std::shared_ptr<Vehicle>> vehicles; // Dynamically store vehicles
 
 public:
-    static VehicleManager* getInstance(); // Public method to access the instance
+    static VehicleManager* getInstance();
 
-    // Other methods for managing vehicles can be added here
-    void addVehicle(const std::string& vehicleName);
-    void printVehicleCount(int num_vehicle);
+    void addVehicle(const std::string& name, const std::string& type);
+    void listVehicles() const;
+    void printVehicleCount() const;
 };
 
 #endif // VEHICLE_MANAGER_H
